068bc26c1c99224a44552fe6108bea53ecefe030
Ion <127430807+IonGadarenco@users.noreply.github.com>
2025-04-07 19:29:11 +0300

    add greating from dana

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World! from dana!");
 
             string SayHi(string text) {
                 return text;
