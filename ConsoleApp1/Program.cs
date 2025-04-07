56d950bce86a596548f70b7edab9717fc72793bd
Ion <127430807+IonGadarenco@users.noreply.github.com>
2025-04-07 19:31:54 +0300

    add greating from ion

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World! from ion!");
 
             string SayHi(string text) {
                 return text;
